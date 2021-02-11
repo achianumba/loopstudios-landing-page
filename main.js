5506469a32ffb12a7f0fcc17844f81f6c5454af9
achianumba <chianumbaarinze@gmail.com>
2021-02-11 23:34:22 +0100

    Add show/hide mobile nav functionality and improve UI for upper sections

--- main.js
+++ main.js
@@ -3,8 +3,36 @@ document.addEventListener("scroll", function (e) {
   let header = document.querySelector(".site__header");
 
   if (heroPosition.getBoundingClientRect().y < -16) {
-    header.classList.add("site__header--dark");
+    header.classList.add("site__header--dark", "site__header--reduced-padding");
   } else {
-    header.classList.remove("site__header--dark");
+    header.classList.remove(
+      "site__header--dark",
+      "site__header--reduced-padding"
+    );
   }
 });
+
+document.querySelector("img.site__nav--toggle").onclick = function () {
+  let header = document.querySelector(".site__header");
+  let nav = document.querySelector(".site__nav");
+  let navBtn = document.querySelector(".site__nav--toggle");
+
+  if (getComputedStyle(nav).display === "none") {
+    header.classList.add("site__header--dark", "site__header--fullscreen");
+    nav.classList.add("site__nav--show");
+    navBtn.src = "./images/icon-close.svg";
+  } else {
+    let heroPosition = document.querySelector(".hero").getBoundingClientRect()
+      .y;
+
+    header.classList.remove("site__header--fullscreen");
+
+    if (heroPosition > -16) {
+      header.classList.remove("site__header--dark");
+    }
+
+    nav.classList.remove("site__nav--show");
+
+    navBtn.src = "./images/icon-hamburger.svg";
+  }
+};
